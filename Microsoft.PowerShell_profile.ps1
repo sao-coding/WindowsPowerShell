6e29edee7b283bc24e32e6549c8daec6e1694097
sao-coding <jeter.nice@gmail.com>
2023-04-20 21:35:54 +0800

    修復 錯誤

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,1 @@
-oh-my-posh init pwsh --config "C:\Users\jeter\Documents\WindowsPowerShell\only.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "C:\Users\jeter\Documents\WindowsPowerShell\only.omp.json"  | Invoke-Expression
